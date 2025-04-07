0ac4bfdf432fe1f6132ced1459e3cc615c80235e
Amulya Jain <amuly.jain@gmail.com>
2025-04-07 14:21:46 -0400

    Calculate the direction of the normal

--- src/sphere.cpp
+++ src/sphere.cpp
@@ -20,13 +20,14 @@ bool sphere::hit(const ray &r, double ray_tmin, double ray_tmax, hit_record &rec
     auto root = (h - sqrtd) / a;
     if (root <= ray_tmin || ray_tmax <= root) {
         root = (h + sqrtd) / a;
-        if (root <= ray_tmin || ray_tmax <= root)
-            return false;
+        if (root <= ray_tmin || ray_tmax <= root) return false;
     }
 
     rec.t = root;
     rec.p = r.at(rec.t);
     rec.normal = (rec.p - center) / radius;
+    vec3 outward_normal = (rec.p - center) / radius;
+    rec.set_face_normal(r, outward_normal);
 
     return true;
 }
