f5267227abec95f52fb7f71d976526d24db792cd
Amulya Jain <amuly.jain@gmail.com>
2025-08-07 00:02:35 -0400

    Final Render (4K)
    Signed-off-by: Amulya Jain <amuly.jain@gmail.com>

--- src/camera.cpp
+++ src/camera.cpp
@@ -17,7 +17,7 @@
 
 constexpr int  WORK_PER_WORKER  = 16;    // number of pixels each worker will render in a single call
 constexpr bool USE_PRETTY_PRINT = true;  // whether to use pretty print or not
-constexpr bool USE_DISPLAY      = false; // whether to use display or not
+constexpr bool USE_DISPLAY      = true; // whether to use display or not
 
 void camera::render_worker(std::atomic<size_t> &next_pixel_idx, const hittable &world, grid &img) const {
     const size_t image_total = image_width * image_height;
